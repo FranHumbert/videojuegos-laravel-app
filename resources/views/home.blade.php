b5bc1d053fd2c53718f2bcde4976cfa9e7e1e702
Francesc <franhumbert@gmail.com>
2025-08-12 18:45:10 +0200

    Primera subida del programa

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+    <h1>Bienvenido a la baase de datos sobre videojuegos y consolas</h1>
+</body>
+</html>
