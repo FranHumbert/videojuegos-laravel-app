1420cb04808b323a2c67f68a6a364044b59d6059
Francesc <franhumbert@gmail.com>
2025-08-26 11:55:44 +0200

    rutas subidas y comprovadas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\VideojuegoController;
+use App\Http\Controllers\PlataformaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Pagina principal
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+//Rutas de recursos para videojuegos
+Route::resource('videojuegos', VideojuegoController::class);
+
+//Rutas de recursos para plataformas
+Route::resource('plataformas', PlataformaController::class);
